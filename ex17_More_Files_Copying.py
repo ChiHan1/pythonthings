0e78e6b719217f443983b218b5aea258084a5bab
chihan1 <Nathan.chiurco@gmail.com>
2016-08-01 14:47:24 -0400

    Changes

--- ex17_More_Files_Copying.py
+++ ex17_More_Files_Copying.py
@@ -3,21 +3,22 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print "Copying from %s to %s" % (from_file, to_file)
+
 
 in_file = open(from_file)
 indata = in_file.read()
 
-print "The input file is %d bytes long" % len(indata)
 
-print "Does the output file exist? %r" % exists(to_file)
+
+
 print "Ready, hit RETURN to continue, CTRL-C to fuck this program up."
 raw_input()
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
 
-print "Alright, all done."
-
+print "Copyed from %s to %s" % (from_file, to_file);
+print "The output file was %d bytes long" % len(to_file)
+print "The input file was %d bytes long" % len(indata)
 out_file.close()
 in_file.close()
